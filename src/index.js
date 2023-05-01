34f2c974ebaca10aa03406b1840e89225609158c
JareJarekus <yaros7553@google.com>
2023-05-02 00:51:23 +0300

    some commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import axios from "axios";
-// console.log(fetch('https://jsonplaceholder.typicode.com/albums')
-// .then(response => {
-//     if (!response.ok){
-//         throw new Error ('Alles kaputt')
-//     }
-//     return response.json();
-// })
-// .then(res => console.log(res))
-// .catch(err => console.log(err))
-// ); 
-
-
-
 const sssdiv = document.querySelector('#sssdiv')
 
 function template() {
@@ -28,17 +14,3 @@ console.log(sssdiv.insertAdjacentHTML("beforeend", `<img src="${thumbnailUrl}" a
 .catch(err => console.log(err))
 };
 template()
-
-
-
-// async function fetch(){
-//     try{
-//         const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
-//         const formatedData = response.data;
-//         return formatedData;
-//     } catch (error){
-//     if(error.code === 'ERR_NETWORK'){
-//         console.log(1);        }
-//     }
-// }
-// fetch();
